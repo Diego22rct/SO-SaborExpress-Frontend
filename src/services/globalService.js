82c0bc29374d049c75db71f2635a9d1ea502df34
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-09-22 15:47:07 -0500

    feat: Add dynamic categories to the sidebar menu

--- src/services/globalService.js
+++ src/services/globalService.js
@@ -3,6 +3,11 @@ const BASE_URL = "http://127.0.0.1:8000";
 export const getProducts = async () => {
     const res = await fetch(`${BASE_URL}/products`);
     const data = await res.json();
-    console.log(data);
+    return data;
+};
+
+export const getCategories = async () => {
+    const res = await fetch(`${BASE_URL}/categories`);
+    const data = await res.json();
     return data;
 };
