9af3770df3a776932961677d21f8bc4ca67b1cc3
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-09-22 15:33:55 -0500

    feat: Update product card component and fetch products from global service

--- src/services/globalService.js
+++ src/services/globalService.js
@@ -2,15 +2,7 @@ const BASE_URL = "http://127.0.0.1:8000";
 
 export const getProducts = async () => {
     const res = await fetch(`${BASE_URL}/products`);
-    return res.json();
-};
-
-export const getCategories = async () => {
-    const res = await fetch(`${BASE_URL}/categories`);
-    return res.json();
-};
-
-export const getProductsByCategory = async (category) => {
-    const res = await fetch(`${BASE_URL}/products/category/${category}`);
-    return res.json();
+    const data = await res.json();
+    console.log(data);
+    return data;
 };
